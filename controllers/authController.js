014b6e38b9c5f5660a6997ea3f5057250f1eebb6
Robert <robertyeakeltroidl@gmail.com>
2019-10-31 23:44:17 -0700

    add login

--- controllers/authController.js
+++ controllers/authController.js
@@ -49,3 +49,21 @@ exports.signup = catchAsync(async (req, res, next) => {
 
   createSendToken(newUser, 201, req, res);
 });
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1) Check if email and password exist
+  if (!email || !password) {
+    return next(new AppError("Please provide email and password!", 400));
+  }
+  // 2) Check if user exists && password is correct
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError("Incorrect email or password", 401));
+  }
+
+  // 3) If everything ok, send token to client
+  createSendToken(user, 200, req, res);
+});
