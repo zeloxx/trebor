965e31ba6ce777e720ab526e3a2a0b808efc5dd5
Robert <robertyeakeltroidl@gmail.com>
2019-10-31 23:43:42 -0700

    add signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -34,3 +34,18 @@ const createSendToken = (user, statusCode, req, res) => {
     }
   });
 };
+
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm
+  });
+
+  const url = `${req.protocol}://${req.get("host")}/me`;
+  // console.log(url);
+  await new Email(newUser, url).sendWelcome();
+
+  createSendToken(newUser, 201, req, res);
+});
