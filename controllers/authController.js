d49ba1ea83b61e4401209aa96f4dff0b9359f989
Robert <robertyeakeltroidl@gmail.com>
2019-10-31 23:50:52 -0700

    add function to restrict actions to users with certain roles

--- controllers/authController.js
+++ controllers/authController.js
@@ -150,3 +150,16 @@ exports.isLoggedIn = async (req, res, next) => {
   }
   next();
 };
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    // roles ['admin']. role='user'
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError("You do not have permission to perform this action", 403)
+      );
+    }
+
+    next();
+  };
+};
