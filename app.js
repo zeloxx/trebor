831aeea3ba3815f18ec867d5d401a1324ad2cb86
Robert <robertyeakeltroidl@gmail.com>
2019-11-03 00:50:59 -0700

    add config.env and server.js

--- app.js
+++ app.js
@@ -3,10 +3,4 @@ const cors = require("cors");
 
 const app = express();
 
-const PORT = 5000;
-
-app.get("/", function(req, res) {
-  res.send("Hello World!");
-});
-
-app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
+module.exports = app;
